4954d990335511c9ed36e2e191fc88379bed4092
darzwaitz <darzwaitz@yahoo.com>
2021-01-29 15:08:44 +0000

    added battle log - final functionality with dymanic classes

--- Vue Project 1 - Game/app.js
+++ Vue Project 1 - Game/app.js
@@ -8,7 +8,8 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
-            winner: null
+            winner: null,
+            logMessages: []
         };
     },
     methods: {
@@ -16,22 +17,26 @@ const app = Vue.createApp({
             this.playerHealth = 100,
             this.monsterHealth = 100,
             this.currentRound = 0,
-            this.winner = null
+            this.winner = null,
+            this.logMessages = []
         },
         attackMonster(){
             this.currentRound++;
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player','attack',attackValue);
             this.attackPlayer();
         },
         attackPlayer(){
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+            this.addLogMessage('monster','attack',attackValue);
         },
         specialAttackMonster(){
             this.currentRound++;
             const attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player','attack',attackValue);
             this.attackPlayer();
         },
         healPlayer(){
@@ -41,7 +46,19 @@ const app = Vue.createApp({
             }else{
             this.playerHealth += healValue;
             }
+            this.addLogMessage('player','heal',healValue);
             this.attackPlayer();
+        },
+        surrender() {
+            this.winner = 'monster'
+        },
+        addLogMessage(who, what, value){
+            //add msg object to start of array list
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            });
         }
     },
     watch:{
