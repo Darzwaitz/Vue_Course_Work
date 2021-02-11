d26091677ddbd0fb0149f9322feda55d24268646
darzwaitz <darzwaitz@yahoo.com>
2021-02-11 21:52:33 +0000

    added first vue cli project - initial directory & files

---
+++ vue-components/Vue-Cli-basic-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>tesht</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {            
+            friends: [
+                { 
+                    id: 'manuel', 
+                    name: 'Manuel Lorenz',
+                    phone: '01234 56789',
+                    email: 'test@test.com'
+                },
+                { 
+                    id: 'julie', 
+                    name: 'Julie Lorenz',
+                    phone: '01333 49876',
+                    email: 'test2@test2.com'
+                }
+            ]
+        };
+    }
+}
+</script>
---
+++ vue-components/Vue-Cli-basic-app/src/main.js
@@ -0,0 +1,7 @@
+//importing a package only needs package name
+import { createApp } from 'vue';
+
+//importing custom file needs relative path
+import App from './App.vue';
+
+createApp(App).mount('#app');
