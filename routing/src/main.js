eb31c9774937987dd6b572f72d82a93a3c7829b4
darzwaitz <darzwaitz@yahoo.com>
2022-02-23 12:38:02 +0000

    query params & multiple route components

--- routing/src/main.js
+++ routing/src/main.js
@@ -6,6 +6,8 @@ import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -18,12 +20,20 @@ const router = createRouter({
         {
           name: 'team-members',
           path: '/teams/:teamId',
-          component: TeamMembers,
-          props: true,
+          // component: TeamMembers,
+          components: { default: TeamsList, footer: TeamsFooter },
+          children: [
+            {
+              name: 'team-members',
+              path: ':teamId',
+              component: TeamMembers,
+              props: true,
+            },
+          ],
         },
       ],
     },
-    { path: '/users', component: UsersList },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
     // { path: '/:notFound(.*)', redirect: '/teams' },
     { path: '/:notFound(.*)', component: NotFound },
   ],
