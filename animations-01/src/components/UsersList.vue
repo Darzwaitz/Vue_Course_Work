407ee012eea1a320163e790b2097052d11e8c8f4
darzwaitz <darzwaitz@yahoo.com>
2022-03-22 22:38:36 +0000

    animating list

---
+++ animations-01/src/components/UsersList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="user in users" :key="user">{{ user }}</li>
+  </ul>
+  <div>
+    <input type="text" />
+    <button>Add User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: ['Darz', 'Johny', 'Sam', 'Alan'],
+    };
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+li {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
