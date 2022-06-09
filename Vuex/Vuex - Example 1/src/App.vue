221f4e436b44271581ada0851c1c484471b77662
darzwaitz <darzwaitz@yahoo.com>
2022-06-09 22:48:37 +0100

    adding initial Composition API example 1 filez

---
+++ Vuex/Vuex - Example 1/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <base-container title="Vuex" v-if="isAuth">
+    <TheCounter />
+    <FavouriteValue />
+    <h3>{{ counter }}</h3>
+    <button @click="addOne">Add 10</button>
+    <ChangeCounter />
+  </base-container>
+  <base-container title="Auth">
+    <UserAuth />
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavouriteValue from './components/FavouriteValue.vue';
+import UserAuth from './components/UserAuth.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavouriteValue,
+    UserAuth,
+  },
+  computed: {
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;
+    },
+  },
+  methods: {
+    addOne() {
+      // this.$store.commit('increase', { value: 10 });
+      // this.$store.commit({
+      //   type: 'increase',
+      //   value: 10,
+      // }
+      this.$store.dispatch({
+        type: 'numbers/increase',
+        value: 10,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
