123d8bae7e22188a26858c61c9ef953d09ed3484
darzwaitz <darzwaitz@yahoo.com>
2022-06-13 23:21:35 +0100

    Composition API - the computed method, imported - functions as read only

--- Composition API/src/App.vue
+++ Composition API/src/App.vue
@@ -1,24 +1,44 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
+    <h2>{{ uName }}</h2>
+    <h3>{{ uAge }}</h3>
     <button @click="increment">Increment x 1</button>
+    <div class="">
+      <input type="text" placeholder="First Name" @input="setFirstName" />
+      <input type="text" placeholder="Last Name" @input="setLastName" />
+    </div>
   </section>
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+// import { reactive } from 'vue';
+import { ref, computed } from 'vue';
 
 // export default {
 //   setup() {
 // const uName = ref('Darz');
-// const uAge = ref(40);
-const user = reactive({
-  name: 'Darz',
-  age: 40,
+const uAge = ref(40);
+const firstName = ref('');
+const lastName = ref('');
+
+const uName = computed(function () {
+  return firstName.value + ' ' + lastName.value;
 });
 
-console.log(user);
+function setFirstName(event) {
+  firstName.value = event.target.value;
+}
+
+function setLastName(event) {
+  lastName.value = event.target.value;
+}
+
+// const user = reactive({
+//   name: 'Darz',
+//   age: 40,
+// });
+
+// console.log(user);
 
 // setTimeout(function () {
 //   user.name = 'Darzer';
@@ -26,7 +46,7 @@ console.log(user);
 // }, 2000);
 
 function increment() {
-  user.age++;
+  uAge.value++;
 }
 
 // return { userName: uName };
