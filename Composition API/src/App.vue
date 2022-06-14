3cc28a44ce71d31ff10c204de4c6af98d0ed2066
darzwaitz <darzwaitz@yahoo.com>
2022-06-14 23:42:17 +0100

    Composition API - v-model 2 way binding, using ref method

--- Composition API/src/App.vue
+++ Composition API/src/App.vue
@@ -4,8 +4,8 @@
     <h3>{{ uAge }}</h3>
     <button @click="increment">Increment x 1</button>
     <div class="">
-      <input type="text" placeholder="First Name" @input="setFirstName" />
-      <input type="text" placeholder="Last Name" @input="setLastName" />
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" v-model="lastName" />
     </div>
   </section>
 </template>
@@ -25,13 +25,13 @@ const uName = computed(function () {
   return firstName.value + ' ' + lastName.value;
 });
 
-function setFirstName(event) {
-  firstName.value = event.target.value;
-}
+// function setFirstName(event) {
+//   firstName.value = event.target.value;
+// }
 
-function setLastName(event) {
-  lastName.value = event.target.value;
-}
+// function setLastName(event) {
+//   lastName.value = event.target.value;
+// }
 
 // const user = reactive({
 //   name: 'Darz',
